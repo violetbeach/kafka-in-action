ef3ea377ccc91571da69d4373604826ebe3a7e0a
Jerry(이재헌) <ti6419@gmail.com>
2023-03-31 08:51:44 +0900

    Kafka 이벤트 전파 추가

--- src/main/java/com/violetbeach/kafkainaction/service/SaveService.java
+++ src/main/java/com/violetbeach/kafkainaction/service/SaveService.java
@@ -8,6 +8,7 @@ import com.violetbeach.kafkainaction.repository.CheckOutRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -16,6 +17,10 @@ import java.util.Date;
 @Service
 @RequiredArgsConstructor
 public class SaveService {
+
+    private final static String CHECKOUT_COMPLETE_TOPIC_NAME = "checkout.complete.v1";
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
 
     private final ObjectMapper objectMapper = new ObjectMapper()
             .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
@@ -28,12 +33,24 @@ public class SaveService {
         checkOutDto.setCheckOutId(checkOutEntity.getCheckOutId());
         checkOutDto.setCreatedAt(new Date(checkOutEntity.getCreatedAt().getTime()));
 
+        sendToKafka(checkOutDto);
+
         return checkOutEntity.getCheckOutId();
     }
 
     private CheckOutEntity saveDatabase(CheckOutDto checkOutDto) {
         CheckOutEntity checkOutEntity = modelMapper.map(checkOutDto, CheckOutEntity.class);
         return checkOutRepository.save(checkOutEntity);
+    }
+
+    private void sendToKafka(CheckOutDto checkOutDto) {
+        try {
+            String jsonInString = objectMapper.writeValueAsString(checkOutDto);
+            kafkaTemplate.send(CHECKOUT_COMPLETE_TOPIC_NAME, jsonInString);
+            log.info("success sendToKafka");
+        } catch (Exception e) {
+            log.error("sendToKafka", e);
+        }
     }
 
 
