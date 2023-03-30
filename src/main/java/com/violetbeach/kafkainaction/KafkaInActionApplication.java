930ea91d17d189b478ede3365ba5a0d6d94bee40
Jerry(이재헌) <ti6419@gmail.com>
2023-03-30 20:39:32 +0900

    Initial commit

---
+++ src/main/java/com/violetbeach/kafkainaction/KafkaInActionApplication.java
@@ -0,0 +1,13 @@
+package com.violetbeach.kafkainaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaInActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaInActionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/violetbeach/kafkainaction/KafkaInActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.violetbeach.kafkainaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaInActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
